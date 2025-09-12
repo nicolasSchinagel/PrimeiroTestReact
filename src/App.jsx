d2a228c82f9b373d27e73100ecafd69224a65706
nicolasschinagel <drivepoweringames@gmail.com>
2025-09-12 19:55:34 -0300

    Revendo git

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Index from "./pages/index.jsx";
 import Musics from "./pages/musics.jsx";
 import Dances from "./pages/dances.jsx";
 import More from "./pages/more.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 function App(){
 return(
    <Router>
